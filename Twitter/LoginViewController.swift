3790d02b8e0bb252bd3a232e3cbb55d40dde8cfb
Yordanos SoIomon <ybsolomon@dons.usfca.edu>
2021-02-26 14:09:00 -0800

    added persistent login

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -17,10 +17,19 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
+    
     @IBAction func onLoginButton(_ sender: Any) {
         let login = "https://api.twitter.com/oauth/request_token"
         
         TwitterAPICaller.client?.login(url: login, success: {
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Could not log in!")
